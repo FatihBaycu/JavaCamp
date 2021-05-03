302c556f0f142f5257e2f0c205b3eeb3a2d81453
FatihBaycu <email@example.com>
2021-05-03 23:40:11 +0300

    Düzenlendi.

--- src/InterfaceAndAbstract/Adapters/MernisServiceAdapter.java
+++ src/InterfaceAndAbstract/Adapters/MernisServiceAdapter.java
@@ -6,9 +6,16 @@ import InterfaceAndAbstract.Entities.Customer;
 public class MernisServiceAdapter implements CustomerCheckService {
     @Override
     public boolean ChcekIfRealPerson(Customer customer) {
-//        KpsPublicSoapClient client=new KpsPublicSoapClient();
+
+        //        KpsPublicSoapClient client=new KpsPublicSoapClient();
 //        return client.TcKimlikNoDogrula(customer.getNatiolanityId(),customer.getFirstName().toUpperCase(),
 //                customer.getLastName().toUpperCase(),customer.getDateOfBirth().getYear());
-return true;
+
+
+        //MERNİS SİSTEMİNİN ÇALIŞMASI İÇİN "MAVEN" YAPIDA BİR PROJE OLMASI GEREKİYOR ANCAK ÖYLE WEB SERVİCE KULLANABİLİRİZ.
+        //BU PROJE STANDART CONSOLE APP JAVA OLDUĞU İÇİN BURADA ÇALIŞMADI FAKAT KODLAR BİREBİR AYNI....
+
+
+        return true;
     }
 }
